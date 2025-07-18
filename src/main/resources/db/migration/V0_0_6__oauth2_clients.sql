f23b6027222b3f00a3adc1777bf9cb9dd643e2c3
Nitin Khaitan <khaitan.nitin@gmail.com>
2025-07-18 17:22:44 +1000

    db script optimization

--- src/main/resources/db/migration/V0_0_6__oauth2_clients.sql
+++ src/main/resources/db/migration/V0_0_6__oauth2_clients.sql
@@ -2,6 +2,7 @@
 -- This replaces the client registration in OAuth2ClientRegistrationConfig.java
 
 -- Messaging Client (messaging-client)
+-- Final redirect URIs support both Group 1 (port 8001) and Group 2 (port 8004)
 INSERT IGNORE INTO `oauth2_registered_client` VALUES (
     '1',
     'messaging-client', 
@@ -11,8 +12,8 @@ INSERT IGNORE INTO `oauth2_registered_client` VALUES (
     'messaging-client',
     'client_secret_basic',
     'authorization_code,refresh_token,client_credentials',
-    'https://local.codzs.com:8004/login/oauth2/code/messaging-client-oidc,https://local.codzs.com:8004/authorized',
-    'https://local.codzs.com:8004/logged-out',
+    'https://local.codzs.com:8001/login/oauth2/code/messaging-client-oidc,https://local.codzs.com:8001/authorized,https://local.codzs.com:8004/login/oauth2/code/messaging-client-oidc,https://local.codzs.com:8004/authorized',
+    'https://local.codzs.com:8001/logged-out,https://local.codzs.com:8004/logged-out',
     'openid,profile,message.read,message.write,user.read',
     '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":true}',
     '{\"@class\":\"java.util.Collections$UnmodifiableMap\", \"settings.token.access-token-format\": {\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\", \"value\":\"self-contained\"}, \"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000]}'
@@ -53,6 +54,7 @@ INSERT IGNORE INTO `oauth2_registered_client` VALUES (
 );
 
 -- mTLS Demo Client (mtls-demo-client)
+-- Final configuration with correct certificate DN and enabled certificate bound tokens
 INSERT IGNORE INTO `oauth2_registered_client` VALUES (
     '4',
     'mtls-demo-client',
@@ -65,6 +67,6 @@ INSERT IGNORE INTO `oauth2_registered_client` VALUES (
     NULL,
     NULL,
     'message.read,message.write',
-    '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":false,\"settings.client.x509-certificate-subject-dn\":\"CN=127.0.0.1,O=Codzs,L=Melbourne,ST=Victoria,C=AU\",\"settings.client.jwk-set-url\":\"https://local.codzs.com:8001/jwks\"}',
-    '{\"@class\":\"java.util.Collections$UnmodifiableMap\", \"settings.token.access-token-format\": {\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\", \"value\":\"self-contained\"}, \"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.x509-certificate-bound-access-tokens\":false}'
+    '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":false,\"settings.client.x509-certificate-subject-dn\":\"EMAILADDRESS=khaitan.nitin@gmail.com, CN=127.0.0.1, O=Codzs, L=Melbourne, ST=Victoria, C=AU\",\"settings.client.jwk-set-url\":\"https://local.codzs.com:8001/jwks\"}',
+    '{\"@class\":\"java.util.Collections$UnmodifiableMap\", \"settings.token.access-token-format\": {\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\", \"value\":\"self-contained\"}, \"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.x509-certificate-bound-access-tokens\":true}'
 ); 
--- src/main/resources/db/migration/V0_0_9__disable_cert_bound_tokens.sql
+++
@@ -1,5 +0,0 @@
--- Enable certificate-bound tokens for mTLS client
-
-UPDATE oauth2_registered_client 
-SET token_settings = '{"@class":"java.util.Collections$UnmodifiableMap", "settings.token.access-token-format": {"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat", "value":"self-contained"}, "settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.authorization-code-time-to-live":["java.time.Duration",3600.000000000],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.x509-certificate-bound-access-tokens":true}'
-WHERE client_id = 'mtls-demo-client';
